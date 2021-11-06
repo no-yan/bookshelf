48165933dee15bdb6ffb906ffbad75152f27b86a
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-11-06 18:12:28 +0900

    add colorpicker

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -1,24 +1,44 @@
 import { Button } from '@chakra-ui/button';
+import { HStack } from '@chakra-ui/layout';
 import React, { useState } from 'react';
 import { ColorBox } from '../components/colorSelector/ColorButton';
+import { ColorPicker } from '../components/colorSelector/ColorPicker';
 import { ColorSlider } from '../components/colorSelector/ColorSlider';
 
-const Sandbox = () => {
+const ButtonSelect = () => {
     const [color, setColor] = useState('red.100');
 
-    const colorScheme = 'red';
-    const [colorValue, setColorValue] = useState(100);
     return (
-        <>
+        <HStack>
             <Button bg={color}>Check The Color</Button>
 
             <ColorBox setValue={setColor} />
+        </HStack>
+    );
+};
+
+const SliderSelect = () => {
+    const colorScheme = 'red';
+    const [colorValue, setColorValue] = useState(100);
+    console.log(colorValue);
+    return (
+        <HStack>
             <Button bg={`${colorScheme}.${colorValue}`}>Check The Color</Button>
             <ColorSlider
                 colorScheme={colorScheme}
                 value={colorValue}
                 setValue={setColorValue}
             />
+        </HStack>
+    );
+};
+
+const Sandbox = (): JSX.Element => {
+    return (
+        <>
+            <ButtonSelect />
+            <SliderSelect />
+            <ColorPicker />
         </>
     );
 };
